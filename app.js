8d60e6794100621ea656e0a347db2107de25a8d1
Rohit <rohitsrao@gmail.com>
2021-10-22 19:45:03 +0200

    Compose file created

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb://db:27017/test', {useNewUrlParser: true})
+  .catch(err => {
+    console.log(err);
+  });
+
+const testSchema = new mongoose.Schema({
+  num1: Number,
+  num2: Number,
+  result: Number,
+});
+
+const Sum = mongoose.model('sum', testSchema);
+
+const sum = new Sum({
+  num1: 5,
+  num2: 3,
+  result: 8,
+});
+sum.save();
+
+Sum.find((err, sums) => {
+  console.log(sums);
+});
 
 app.get('/', function (req, res){
   res.sendFile(__dirname + '/index.html');
